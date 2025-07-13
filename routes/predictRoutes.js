4c649cbfd8533838580e3013dcb6f4f57b5333e4
Damilare <da9783790@gmil.com>
2025-07-13 17:01:10 +0100

    update the predictController and Route commit

--- routes/predictRoutes.js
+++ routes/predictRoutes.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const { predictDisease, getUserPredictions, deletePredictionById } = require('../controllers/predictController');
+const multer = require('../config/multerConfig');
+const {
+  predictDisease,
+  getUserPredictions,
+  deletePredictionById,
+} = require('../controllers/predictController');
 const { protect } = require('../middleware/authMiddleware');
 
 const router = express.Router();
 
-router.post('/', protect, predictDisease); 
+router.post('/', protect, multer.single('image'), predictDisease);
+
 router.get('/history', protect, getUserPredictions);
-router.delete('/:id', protect, deletePredictionById); 
+
+router.delete('/:id', protect, deletePredictionById);
 
 module.exports = router;
